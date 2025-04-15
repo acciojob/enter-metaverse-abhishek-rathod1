fc693a57b262ef0bd906e5fd78bb6c6ad5491b86
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-15 19:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-let body = document.getElementById("my-body");
+// let body = document.getElementById("my-body");
 function buttonClick() {
 	let statusPara = document.getElementById("status");
 	
-	const header = document.createElement("h1");
+	const header = document.createElement('h1');
 	header.textContent = "Entered Metaverse"
 	header.id = "status";
 
