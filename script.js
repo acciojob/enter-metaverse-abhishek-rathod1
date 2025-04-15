3aa2b29d1161148ef34a09d57b4710b3de0bd4c8
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-15 19:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let body = document.getElementById("my-body");
+function buttonClick() {
+	let statusPara = document.getElementById("status");
+	
+	const header = document.createElement("h1");
+	header.textContent = "Entered Metaverse"
+	header.id = "status";
+
+	//replace child
+	statusPara.parentNode.replaceChild(header, statusPara);
+	
+	
+}
